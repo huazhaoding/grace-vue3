6d66415a13f29e889cf1fac05e03e0ef312066db
GETAWAY <634866553@qq.com>
2024-02-14 21:44:24 +0800

    DictTag组件，当value没有匹配的值时，展示value

--- src/components/DictTag/index.vue
+++ src/components/DictTag/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <template v-for="(item, index) in options">
-      <template v-if="(dictSort && values==(''+item.dictSort)) ||values.includes(item.value) ">
+      <template
+        v-if="
+          (dictSort && values == '' + item.dictSort) ||
+          values.includes(item.value)
+        "
+      >
         <span
           v-if="item.elTagType == 'default' || item.elTagType == ''"
           :key="item.value"
           :index="index"
           :class="item.elTagClass"
-        >{{ item.label }}</span>
+          >{{ item.label + " " }}</span
+        >
         <el-tag
           v-else
           :disable-transitions="true"
@@ -15,13 +21,21 @@
           :index="index"
           :type="item.elTagType === 'primary' ? '' : item.elTagType"
           :class="item.elTagClass"
-        >{{ item.label }}</el-tag>
+        >
+          {{ item.label + " " }}</el-tag
+        >
       </template>
+    </template>
+    <template v-if="unmatch && showValue">
+      {{ unmatchArray | handleArray }}
     </template>
   </div>
 </template>
 
 <script setup>
+// // 记录未匹配的项
+const unmatchArray = ref([]);
+
 const props = defineProps({
   // 数据
   options: {
@@ -31,20 +45,49 @@ const props = defineProps({
   // 当前的值
   value: [Number, String, Array],
   // 是否通过dictSort
-  dictSort:{
+  dictSort: {
     type: Boolean,
-    default:false
-  }
-})
+    default: false,
+  },
+  // 当未找到匹配的数据时，显示value
+  showValue: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 const values = computed(() => {
-  if (props.value !== null && typeof props.value !== 'undefined') {
+  if (props.value !== null && typeof props.value !== "undefined") {
     return Array.isArray(props.value) ? props.value : [String(props.value)];
   } else {
     return [];
   }
-})
-
+});
+const unmatch = computed(() => {
+  unmatchArray.value = [];
+  if (props.value !== null && typeof props.value !== "undefined") {
+    // 传入值为非数组
+    if (!Array.isArray(props.value)) {
+      if (props.options.some((v) => v.value == props.value)) return false;
+      unmatchArray.value.push(props.value);
+      return true;
+    }
+    // 传入值为Array
+    props.value.forEach((item) => {
+      if (!props.options.some((v) => v.value == item))
+        unmatchArray.value.push(item);
+    });
+    return true;
+  }
+  // 没有value不显示
+  return false;
+});
+function handleArray(array) {
+  if (array.length === 0) return "";
+  return array.reduce((pre, cur) => {
+    return pre + " " + cur;
+  });
+}
 </script>
 
 <style scoped>
