e89ddb000c40aab47dbe2205fe5c0b4cd0df225d
GETAWAY <634866553@qq.com>
2024-02-15 17:06:00 +0800

    富文本编辑器支持fileSize和type

--- src/components/Editor/QuillEditor/index.vue
+++ src/components/Editor/QuillEditor/index.vue
@@ -1,12 +1,26 @@
 <template>
+  <div>
+    <el-upload
+      :action="uploadUrl"
+      :before-upload="handleBeforeUpload"
+      :on-success="handleUploadSuccess"
+      :on-error="handleUploadError"
+      name="file"
+      :show-file-list="false"
+      :headers="headers"
+      class="editor-img-uploader"
+      v-if="type == 'url'"
+    >
+      <i ref="uploadRef" class="editor-img-uploader"></i>
+    </el-upload>
+  </div>
   <div class="editor">
     <quill-editor
+      ref="quillEditorRef"
       v-model:content="content"
-      :options="options"
       contentType="html"
       @textChange="(e) => $emit('update:modelValue', content)"
-      :modules="modules"
-      :toolbar="toolbar"
+      :options="options"
       :style="styles"
     />
   </div>
@@ -18,6 +32,13 @@ import BlotFormatter from "quill-blot-formatter/dist/BlotFormatter";
 import ImageUploader from "quill-image-uploader";
 import request from "@/utils/request";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import { getToken } from "@/utils/auth";
+const { proxy } = getCurrentInstance();
+const quillEditorRef = ref();
+const uploadUrl = ref(import.meta.env.VITE_APP_BASE_API + "/common/upload"); // 上传的图片服务器地址
+const headers = ref({
+  Authorization: "Bearer " + getToken()
+})
 
 const props = defineProps({
   /* 编辑器的内容 */
@@ -29,8 +50,8 @@ const props = defineProps({
     type: Number,
     default: null,
   },
-  // 大小限制(MB)
-  fileSize: {
+   /* 上传文件大小限制(MB) */
+   fileSize: {
     type: Number,
     default: 5,
   },
@@ -38,6 +59,11 @@ const props = defineProps({
   fileType: {
     type: Array,
     default: () => ["png", "jpg", "jpeg", "ico"],
+  },
+  /* 类型（base64格式、url格式） */
+  type: {
+    type: String,
+    default: "url",
   },
   /* 只读 */
   readOnly: {
@@ -52,7 +78,7 @@ const props = defineProps({
   /*工具栏 */
   toolbar: {
     type: Array,
-    default: () =>[
+    default: () => [
       ["bold", "italic", "underline", "strike"], // 加粗 斜体 下划线 删除线
       ["blockquote", "code-block"], // 引用  代码块
       [{ list: "ordered" }, { list: "bullet" }], // 有序、无序列表
@@ -61,8 +87,8 @@ const props = defineProps({
       [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
       [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
       [{ align: [] }], // 对齐方式
-      ["clean"] ,                                       // 清除文本格式
-      ["link", "image", "video"]                       // 链接、图片、视频
+      ["clean"], // 清除文本格式
+      ["link", "image", "video"], // 链接、图片、视频
     ],
   },
 });
@@ -72,7 +98,6 @@ const options = ref({
   placeholder: "请输入内容呀",
   readOnly: props.readOnly,
 });
-const { proxy } = getCurrentInstance();
 const styles = computed(() => {
   let style = {};
   if (props.minHeight) {
@@ -89,14 +114,71 @@ watch(
   () => props.modelValue,
   (v) => {
     if (v !== content.value) {
-    document.querySelectorAll('.blot-formatter__overlay').forEach((item, index, arr) => {
-        item.style.display="none";
-    })
+      document
+        .querySelectorAll(".blot-formatter__overlay")
+        .forEach((item, index, arr) => {
+          item.style.display = "none";
+        });
       content.value = v === undefined ? "<p></p>" : v;
     }
   },
   { immediate: true }
 );
+
+// 如果设置了上传地址则自定义图片上传事件
+onMounted(() => {
+  if (props.type == 'url') {
+    let quill = quillEditorRef.value.getQuill();
+    let toolbar = quill.getModule("toolbar");
+    toolbar.addHandler("image", (value) => {
+      if (value) {
+        proxy.$refs.uploadRef.click();
+      } else {
+        quill.format("image", false);
+      }
+    });
+  }
+});
+
+// 上传前校检格式和大小
+function handleBeforeUpload(file) {
+  const type = ["image/jpeg", "image/jpg", "image/png", "image/svg"];
+  const isJPG = type.includes(file.type);
+  //检验文件格式
+  if (!isJPG) {
+    proxy.$modal.msgError(`图片格式错误!`);
+    return false;
+  }
+  // 校检文件大小
+  if (props.fileSize) {
+    const isLt = file.size / 1024 / 1024 < props.fileSize;
+    if (!isLt) {
+      proxy.$modal.msgError(`上传文件大小不能超过 ${props.fileSize} MB!`);
+      return false;
+    }
+  }
+  return true;
+}
+// 上传成功处理
+function handleUploadSuccess(res, file) {
+  // 如果上传成功
+  if (res.code == 200) {
+    // 获取富文本实例
+    let quill = toRaw(quillEditorRef.value).getQuill();
+    // 获取光标位置
+    let length = quill.selection.savedRange.index;
+    // 插入图片，res.url为服务器返回的图片链接地址
+    quill.insertEmbed(length, "image", import.meta.env.VITE_APP_BASE_API + res.fileName);
+    // 调整光标到最后
+    quill.setSelection(length + 1);
+  } else {
+    proxy.$modal.msgError("图片插入失败");
+  }
+}
+// 上传失败处理
+function handleUploadError() {
+  proxy.$modal.msgError("图片插入失败");
+}
 
 const modules = ref([
   {
@@ -132,13 +214,13 @@ const modules = ref([
     module: BlotFormatter,
     options: {
       overlay: {
-    className: 'blot-formatter__overlay',
-    style: {
-      position: 'absolute',
-      boxSizing: 'border-box',
-      border: '1px dashed red',
-    }
-  }
+        className: "blot-formatter__overlay",
+        style: {
+          position: "absolute",
+          boxSizing: "border-box",
+          border: "1px dashed red",
+        },
+      },
     },
   },
 ]);
@@ -184,6 +266,11 @@ const isAllowFile = (rawFile) => {
 </script>
 
 <style>
+
+.editor-img-uploader {
+  display: none;
+}
+
 .editor,
 .ql-toolbar {
   white-space: pre-wrap !important;
