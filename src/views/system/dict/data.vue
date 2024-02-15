ecfc01cbf4c030ba1120689d2f67b6040558027d
GETAWAY <634866553@qq.com>
2024-02-15 18:08:53 +0800

    优化pagination样式

--- src/views/system/dict/data.vue
+++ src/views/system/dict/data.vue
@@ -207,8 +207,8 @@ const data = reactive({
   queryParams: {
     pageNum: 1,
     pageSize: 10,
-    dictLabel: undefined,
     dictType: undefined,
+    dictLabel: undefined,
     status: undefined
   },
   rules: {
