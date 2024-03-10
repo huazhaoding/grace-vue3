8e75fb3ddf1b38055deb7cec32b00cb3e528d47b
GETAWAY <634866553@qq.com>
2024-03-10 14:49:19 +0800

    代码编辑语言切换

--- src/views/cms/web/theme/themeEdit.vue
+++ src/views/cms/web/theme/themeEdit.vue
@@ -2,13 +2,21 @@
   <div class="app-container">
     <el-row :gutter="20">
       <el-col :span="3" class="el-col">
+        <el-select v-model="language" @change="langChange" placeholder="代码模式" size="large">
+          <el-option
+            v-for="(item, key) in languages"
+            :key="key"
+            :label="key"
+            :value="key"
+          />
+        </el-select>
         <el-button
           class="save-btn"
           type="primary"
           plain
           icon="Edit"
           v-hasPermi="['cms:theme:edit']"
-          :disabled="activeUrl==''"
+          :disabled="activeUrl == ''"
           @click="saveEditHandle"
           >保存主题</el-button
         >
@@ -25,7 +33,7 @@
           :extensions="extensions"
           basic
           @changes="onChanges"
-          :disabled="activeUrl==''"
+          :disabled="activeUrl == ''"
       /></el-col>
       <el-col :span="24"> 底部 </el-col>
     </el-row>
@@ -39,24 +47,30 @@ import { javascript } from "@codemirror/lang-javascript";
 import { css } from "@codemirror/lang-css";
 import { json } from "@codemirror/lang-json";
 import { oneDark } from "@codemirror/theme-one-dark";
-import { getThemeTree, getThemeFileContent,updateThemeFileContent } from "@/api/cms/theme";
+import {
+  getThemeTree,
+  getThemeFileContent,
+  updateThemeFileContent,
+} from "@/api/cms/theme";
 const { proxy } = getCurrentInstance();
 const route = useRoute();
 const content = ref(null);
 const codeRef = ref(null);
 const themeTreeData = ref([]);
-const activeUrl=ref("");
-const config = reactive({
-  language: "html",
-  theme: "oneDark",
-});
-const themes = { oneDark };
+const activeUrl = ref("");
+const extensions = ref([html(),oneDark]); 
+const language=ref("html");
+
 const languages = {
   html: html(),
   javascript: javascript(),
   css: css(),
   json: json(),
 };
+
+function langChange(value){
+  extensions.value=[languages[value],oneDark];
+}
 
 function handleNodeClick(data) {
   if (!data.parent) {
@@ -67,17 +81,16 @@ function handleNodeClick(data) {
           //设置光标到头部 防止超出范围后无法设置内容
           codeRef.value.setCursor(0);
           content.value = res.msg;
-          activeUrl.value=data.id;
+          activeUrl.value = data.id;
         } else {
           proxy.$message.error(res.msg);
         }
       });
+    } else {
+      activeUrl.value = "";
+      content.value = "";
+      proxy.$message.error("不支持类型,请选择html|js|css|txt|json");
     }
-  else{
-    activeUrl.value="";
-    content.value = "";
-    proxy.$message.error("不支持类型,请选择html|js|css|txt|json");
-  }
   }
 }
 
@@ -89,35 +102,23 @@ getThemeTree(route.params.webName).then((res) => {
   }
 });
 
-const extensions = computed(() => {
-  const result = [];
-  result.push(languages[config.language]);
-  if (themes[config.theme]) {
-    result.push(themes[config.theme]);
-  }
-  return result;
-});
-
 const defaultProps = {
   children: "children",
   label: "name",
 };
 
-
 const emit = defineEmits(["codeChange"]);
-const onChanges = () => {
+const onChanges = () => {};
 
-};
-
-function saveEditHandle(){
-  const poData={"path":activeUrl.value,"fileContent":content.value}
-  updateThemeFileContent(route.params.webName,poData).then((res) => {
-  if (res.code === 200) {
-    proxy.$modal.msgSuccess(res.msg);
-  } else {
-    proxy.$message.error(res.msg);
-  }
-});
+function saveEditHandle() {
+  const poData = { path: activeUrl.value, fileContent: content.value };
+  updateThemeFileContent(route.params.webName, poData).then((res) => {
+    if (res.code === 200) {
+      proxy.$modal.msgSuccess(res.msg);
+    } else {
+      proxy.$message.error(res.msg);
+    }
+  });
 }
 </script>
 
@@ -128,7 +129,7 @@ function saveEditHandle(){
 .save-btn {
   margin-left: 40px;
 }
-.code-content{
+.code-content {
   height: 85vh;
   background-color: black;
   overflow-y: scroll;
