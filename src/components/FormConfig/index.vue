315d0f7890cd5336b016b26566147fbff8e33ef2
zhige <634866553@qq.com>
2024-05-29 00:13:17 +0800

    添加说明

--- src/components/FormConfig/index.vue
+++ src/components/FormConfig/index.vue
@@ -3,25 +3,34 @@
     <el-tabs v-model="activeTab" type="border-card">
       <el-tab-pane v-for="(tabItem, i) in configForm.tabData" :key="i" :name="'tab_' + i" :label="tabItem.label">
         <el-form-item v-for="(formItem, j) in tabItem.formData" :key="j" :label="formItem.label" :prop="formItem.prop">
+          <!-- 提示 -->
           <div v-if="formItem.type === 'msg'">
             {{ formData[formItem.prop] }}
           </div>
+          <!-- 开关 -->
           <el-switch v-else-if="formItem.type === 'switch'" v-model="formData[formItem.prop]"></el-switch>
+          <!-- 输入 -->
           <el-input v-else-if="formItem.type === 'input'" v-model="formData[formItem.prop]" type="text"
             :placeholder="formItem.placeholder"></el-input>
+          <!-- 长文本 -->
           <el-input v-else-if="formItem.type === 'textarea'" type="textarea" v-model="formData[formItem.prop]"
             :placeholder="formItem.placeholder" rows="3"></el-input>
+          <!-- 上传图片 -->
           <image-upload v-else-if="formItem.type === 'imgUpload'" v-model="formData[formItem.prop]"
             :limit="formItem.limit" :fileSize="formItem.fileSize" />
+          <!-- 数字 -->
           <el-input-number v-else-if="formItem.type === 'number'" v-model="formData[formItem.prop]"
             controls-position="right" :min="0" :max="100000000000" :precision="0" :step="1">
           </el-input-number>
+          <!-- 单选 -->
           <el-radio-group v-else-if="formItem.type === 'radio'" v-model="formData[formItem.prop]">
             <el-radio v-for="dict in formItem.dicts" :key="dict.value" :label="dict.value">{{ dict.label
             }}</el-radio></el-radio-group>
+            <!-- 下拉 -->
           <config-select v-else-if="formItem.type === 'select'" v-model="formData[formItem.prop]"
             :placeholder="formItem.placeholder" :dicts="formItem.dicts">
           </config-select>
+          <!-- 多选 -->
           <config-checkbox v-else-if="formItem.type === 'checkbox'" v-model="formData[formItem.prop]"
             :dicts="formItem.dicts"></config-checkbox>
         </el-form-item>
@@ -67,7 +76,6 @@ const data = reactive({
 });
 
 const { formData } = toRefs(data);
-
   if (props.configGroup) {
     proxy.getConfigValueMap(props.configGroup).then((response) => {
       formData.value = response.data;
