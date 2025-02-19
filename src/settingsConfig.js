ca0ccd0240aee7e52203c4fc893142b2a108b6b4
GETAWAY <634866553@qq.com>
2025-02-19 19:26:54 +0800

    1

---
+++ src/settingsConfig.js
@@ -0,0 +1,23 @@
+const headConfig = {
+  'title': 'application/json;charset=UTF-8',
+  'author': 'Bearer ' + localStorage.getItem('token'),
+  'keywords': 'Bearer ' + localStorage.getItem('token'),
+  'description':'Bearer ' + localStorage.getItem('token'),
+  'icon': 'https://avatars.githubusercontent.com/u/26362587?s=200&v=4'
+}
+
+const loginConfig = {
+    'back': 'application/json;charset=UTF-8',
+    'notice': 'Bearer ' + localStorage.getItem('token'),
+    'keywords': 'Bearer ' + localStorage.getItem('token'),
+    'description':'Bearer ' + localStorage.getItem('token'),
+    'icon': 'https://avatars.githubusercontent.com/u/26362587?s=200&v=4'
+  }
+
+const footerConfig = {
+    'back': 'application/json;charset=UTF-8',
+    'notice': 'Bearer ' + localStorage.getItem('token'),
+    'keywords': 'Bearer ' + localStorage.getItem('token'),
+    'description':'Bearer ' + localStorage.getItem('token'),
+    'icon': 'https://avatars.githubusercontent.com/u/26362587?s=200&v=4'
+  }
