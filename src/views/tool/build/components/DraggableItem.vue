12dda5d4aadb9257fc2d419c3c1aa8e44a07379f
zhige <634866553@qq.com>
2025-06-21 14:08:41 +0800

    1

--- src/views/tool/build/components/DraggableItem.vue
+++ src/views/tool/build/components/DraggableItem.vue
@@ -1,18 +1,33 @@
 <template>
   <!-- 是表单组件 -->
-    
+
   <!-- 不是表单组件 -->
 
-  
-<render :key="element.tag" :conf="element" v-model="element.attr.defaultValue" />
+  <!-- 布局组件 -->
+
+  <!-- 选择组件 -->
 
 
-  
+  <render :key="element.tag" :conf="element" v-model="element.attr.defaultValue" />
+
+
+  <!-- <div class="drawing-item-operate">
+    <span class="drawing-item-copy" title="复制" @click.stop="copyItem(element)">
+      <el-icon>
+        <CopyDocument />
+      </el-icon>
+    </span>
+    <span class="drawing-item-delete" title="删除" @click.stop="deleteItem(index)">
+      <el-icon>
+        <Delete />
+      </el-icon>
+    </span>
+  </div> -->
 </template>
 <script setup name="DraggableItem">
 import draggable from "vuedraggable/dist/vuedraggable.common";
 import render from '@/utils/generator/render'
-import { watch } from "vue";
+const emits = defineEmits(['activeItem', 'copyItem', 'deleteItem'])
 const props = defineProps({
   element: Object,
   index: Number,
@@ -26,9 +41,8 @@ const props = defineProps({
 
 const className = ref('')
 const draggableItemRef = ref(null)
-const emits = defineEmits(['activeItem', 'copyItem', 'deleteItem'])
 watch(() => props.element, (val) => {
-},{immediate: true,deep:true})
+}, { immediate: true, deep: true })
 function activeItem(item) {
   emits('activeItem', item)
 }
