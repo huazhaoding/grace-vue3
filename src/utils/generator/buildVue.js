435ad8c96de66f8de774c349749842f86bf503f3
Getawy <634866553@qq.com>
2025-07-13 16:07:56 +0800

    代码同步

--- src/utils/generator/buildVue.js
+++ src/utils/generator/buildVue.js
@@ -7,7 +7,9 @@ export function vueTemplate(str) {
   </template>`;
 }
 // 脚本
-export function vueScript(str) {
+export function vueScript(methods,lifeCycles) {
+  let str = generateJavaScript(methods);
+  str += generateJavaScript(lifeCycles);
   return `<script setup>
     ${str}
   </script>`;
@@ -28,6 +30,11 @@ export function makeUpHtml(conf, type) {
   return html;
 }
 
+/**
+ * 列表生成组件
+ * @param {Array} components - 组件列表
+ * @returns {string} - 生成的 HTML 列表
+ */
 function listGenerateComponent(components) {
   let componentStr = "";
   for (let i = 0; i < components.length; i++) {
@@ -56,7 +63,7 @@ function generateComponent(config) {
     hedgeItem.slots.default.slotOptions.push(config);
     return generateComponent(hedgeItem);
   } else {
-    const { tag, attr, slots,events } = config;
+    const { tag, attr, slots, events } = config;
     const simplify = simplifyItemAttr(attr);
     // 构建插槽内容
     let slotContent = "";
@@ -77,12 +84,15 @@ function generateComponent(config) {
         }
       }
     }
-    let eventStr="";
+    let eventStr = "";
     //构建事件
-    if (events) { 
-     eventStr= Object.entries(events).filter(([key, value]) => value !== undefined && value.used && value.value)
-      .map(([key, value]) => `@${key}="${value.functionName}"`)
-      .join(" ")
+    if (events) {
+      eventStr = Object.entries(events)
+        .filter(
+          ([key, value]) => value !== undefined && value.used && value.value
+        )
+        .map(([key, value]) => `@${key}="${value.functionName}"`)
+        .join(" ");
     }
     // 返回完整的组件代码
     return `<${tag} ${attributes} ${eventStr}>
@@ -131,6 +141,14 @@ function simplifyItemAttr(item) {
  *
  *
  */
+
+function generateJavaScript(methods) {
+  const jsContent = Object.entries(methods)
+    .filter(([key, value]) => value !== undefined && value.used)
+    .map(([key, value]) => value.value)
+    .join("\n");
+  return jsContent;
+}
 
 const components = [
   {
@@ -736,74 +754,74 @@ const components2 = [
         ],
       },
     },
-     events: {
-        blur: {
-          used: true,
-          label: "当选择器的输入框失去焦点时触发",
-          functionName: "handleBlur",
-          value: function (item) {},
-        },
-        focus: {
-          used: false,
-          label: "当选择器的输入框获得焦点时触发",
-          functionName: "handleFocus",
-          value: function (item) {},
-        },
-        change: {
-          used: false,
-          label: "仅当 modelValue 改变时，当输入框失去焦点或用户按Enter时触发",
-          functionName: "handleChange",
-          value: function (item) {},
-        },
-        input: {
-          used: false,
-          label: "在 Input 值改变时触发",
-          functionName: "handleInput",
-          value: function (item) {},
-        },
-        clear: {
-          used: false,
-          label: "在点击由 clearable 属性生成的清空按钮时触发",
-          functionName: "handleClear",
-          value: function (item) {},
-        },
-        keydown: {
-          used: false,
-          label: "按下键时触发",
-          functionName: "handleKeydown",
-          value: function () {},
-        },
-        mouseleave: {
-          used: false,
-          label: "当鼠标进入输入框时触发",
-          functionName: "handleMouseleave",
-          value: function (item) {},
-        },
-        mouseenter: {
-          used: false,
-          label: "当鼠标离开输入框时触发",
-          functionName: "handleMouseenter",
-          value: function (item) {},
-        },
-        compositionstart: {
-          used: false,
-          label: "输入法输入开始时触发",
-          functionName: "handleCompositionstart",
-          value: function (item) {},
-        },
-        compositionupdate: {
-          used: false,
-          label: "输入法输入改变时触发",
-          functionName: "handleCompositionupdate",
-          value: function (item) {},
-        },
-        compositionend: {
-          used: false,
-          label: "输入法输入完成时触发",
-          functionName: "handleCompositionend",
-          value: function (item) {},
-        },
+    events: {
+      blur: {
+        used: true,
+        label: "当选择器的输入框失去焦点时触发",
+        functionName: "handleBlur",
+        value: function (item) {},
       },
+      focus: {
+        used: false,
+        label: "当选择器的输入框获得焦点时触发",
+        functionName: "handleFocus",
+        value: function (item) {},
+      },
+      change: {
+        used: false,
+        label: "仅当 modelValue 改变时，当输入框失去焦点或用户按Enter时触发",
+        functionName: "handleChange",
+        value: function (item) {},
+      },
+      input: {
+        used: false,
+        label: "在 Input 值改变时触发",
+        functionName: "handleInput",
+        value: function (item) {},
+      },
+      clear: {
+        used: false,
+        label: "在点击由 clearable 属性生成的清空按钮时触发",
+        functionName: "handleClear",
+        value: function (item) {},
+      },
+      keydown: {
+        used: false,
+        label: "按下键时触发",
+        functionName: "handleKeydown",
+        value: function () {},
+      },
+      mouseleave: {
+        used: false,
+        label: "当鼠标进入输入框时触发",
+        functionName: "handleMouseleave",
+        value: function (item) {},
+      },
+      mouseenter: {
+        used: false,
+        label: "当鼠标离开输入框时触发",
+        functionName: "handleMouseenter",
+        value: function (item) {},
+      },
+      compositionstart: {
+        used: false,
+        label: "输入法输入开始时触发",
+        functionName: "handleCompositionstart",
+        value: function (item) {},
+      },
+      compositionupdate: {
+        used: false,
+        label: "输入法输入改变时触发",
+        functionName: "handleCompositionupdate",
+        value: function (item) {},
+      },
+      compositionend: {
+        used: false,
+        label: "输入法输入完成时触发",
+        functionName: "handleCompositionend",
+        value: function (item) {},
+      },
+    },
   },
 ];
 console.log(listGenerateComponent(components));
